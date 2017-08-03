037235075807745b7096530ebe53c08517fd280b
Salar Ather <salar.ather@richmond.edu>
2017-08-03 06:48:05 +0500

    loaded data and played around with it to see what it looks like

---
+++ language_translation.py
@@ -0,0 +1,33 @@
+#I'll be training a sequence to sequence model on a dataset of English 
+#and French sentences that can translate new sentences from English to French
+
+
+#Since translating the whole language of English to French will take lots of time to train, 
+#I will be using a small portion of the English corpus.
+
+import helper
+
+
+source_path = 'data/small_vocab_en'
+target_path = 'data/small_vocab_fr'
+source_text = helper.load_data(source_path)
+target_text = helper.load_data(target_path)
+
+
+#Playing around with view_sentence_range to view different parts of the data.
+import numpy as np
+
+print('Dataset Stats')
+print('Roughly the number of unique words: {}'.format(len({word: None for word in source_text.split()})))
+
+sentences = source_text.split('\n')
+word_counts = [len(sentence.split()) for sentence in sentences]
+print('Number of sentences: {}'.format(len(sentences)))
+print('Average number of words in a sentence: {}'.format(np.average(word_counts)))
+
+print()
+print('English sentences {} to {}:'.format(*view_sentence_range))
+print('\n'.join(source_text.split('\n')[view_sentence_range[0]:view_sentence_range[1]]))
+print()
+print('French sentences {} to {}:'.format(*view_sentence_range))
+print('\n'.join(target_text.split('\n')[view_sentence_range[0]:view_sentence_range[1]]))
