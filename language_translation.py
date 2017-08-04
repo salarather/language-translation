7426533bf40cfaed4d4132a26002246fe87ca99a
Salar Ather <salar.ather@richmond.edu>
2017-08-04 19:14:23 +0500

    finished writing methods for preprocessing of encoder and decoder input data

--- language_translation.py
+++ language_translation.py
@@ -16,7 +16,7 @@ target_text = helper.load_data(target_path)
 
 #Playing around with view_sentence_range to view different parts of the data.
 import numpy as np
-
+view_sentence_range = (0, 10)
 print('Dataset Stats')
 print('Roughly the number of unique words: {}'.format(len({word: None for word in source_text.split()})))
 
@@ -31,3 +31,53 @@ print('\n'.join(source_text.split('\n')[view_sentence_range[0]:view_sentence_ran
 print()
 print('French sentences {} to {}:'.format(*view_sentence_range))
 print('\n'.join(target_text.split('\n')[view_sentence_range[0]:view_sentence_range[1]]))
+
+
+#Implementing PreProcessing functions
+
+def text_to_ids(source_text, target_text, source_vocab_to_int, target_vocab_to_int):
+    """
+    Convert source and target text to proper word ids
+    :param source_text: String that contains all the source text.
+    :param target_text: String that contains all the target text.
+    :param source_vocab_to_int: Dictionary to go from the source words to an id
+    :param target_vocab_to_int: Dictionary to go from the target words to an id
+    :return: A tuple of lists (source_id_text, target_id_text)
+    """
+    
+    source_id_text = [[source_vocab_to_int[word] for word in sentence.split()] for sentence in source_text.split('\n')]
+    target_id_text = [[target_vocab_to_int[word] for word in sentence.split()] + [target_vocab_to_int['<EOS>']] for sentence in target_text.split('\n')]
+    return (source_id_text, target_id_text)
+
+
+
+def model_inputs():
+    """
+    Create TF Placeholders for input, targets, learning rate, and lengths of source and target sequences.
+    :return: Tuple (input, targets, learning rate, keep probability, target sequence length,
+    max target sequence length, source sequence length)
+    """
+    
+    inputs = tf.placeholder(tf.int32, [None, None], name='input')
+    targets = tf.placeholder(tf.int32, [None, None], name='targets')
+    learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+    keep_probability = tf.placeholder(tf.float32, name = 'keep_prob')
+    target_sequence_length = tf.placeholder(tf.int32, (None,), name='target_sequence_length')
+    max_target_sequence_length = tf.reduce_max(target_sequence_length, name='max_target_sequence_length')
+    source_sequence_length = tf.placeholder(tf.int32, (None,), name='source_sequence_length')
+    return inputs, targets, learning_rate, keep_probability, target_sequence_length, max_target_sequence_length, source_sequence_length
+
+
+def process_decoder_input(target_data, target_vocab_to_int, batch_size):
+    """
+    Preprocess target data for encoding
+    :param target_data: Target Placehoder
+    :param target_vocab_to_int: Dictionary to go from the target words to an id
+    :param batch_size: Batch Size
+    :return: Preprocessed target data
+    """
+    
+    ending = tf.strided_slice(target_data, [0, 0], [batch_size, -1], [1, 1])
+    dec_input = tf.concat([tf.fill([batch_size, 1], target_vocab_to_int['<GO>']), ending], 1)
+
+    return dec_input
